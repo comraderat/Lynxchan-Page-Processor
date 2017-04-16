620bb66deaf9e50f1f3a9aa5ab5e1704db28e10d
ComradeRat <comraderat@gmail.com>
2017-04-16 15:16:33 +0200

    Removed test stuff

--- PreprocessorHook.js
+++ PreprocessorHook.js
@@ -1,19 +1,8 @@
 'use strict';
 
-var test = require("../engine/domManipulator/common.js");
-exports.init=function(){
-	test.originalloadSettings = test.loadSettings;
-	test.loadSettings = function(){
-		console.log("Loading settings lol");
-		test.originalloadSettings();
-	}
-	console.log("Modified Version:");
-	console.log(test.loadSettings.toString());
-	console.log("Original Version:");
-	console.log(test.originalloadSettings.toString());
-}
 
-/*
+
+/* //Original Testcode
 var A = require("../addons/testbehaviourA.js");
 var B = require("../addons/testbehaviourB.js");
 
