0612d7ad31f84a06b859c2b061e9afd6fbc9abe5
ComradeRat <comraderat@gmail.com>
2017-04-16 15:13:31 +0200

    More tests

--- PreprocessorHook.js
+++ PreprocessorHook.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var test = require("../engine/domManipulator/common.js");
+exports.init=function(){
+	test.originalloadSettings = test.loadSettings;
+	test.loadSettings = function(){
+		console.log("Loading settings lol");
+		test.originalloadSettings();
+		console.log("Modified Version:");
+		console.log(test.loadSettings.toString());
+		console.log("Original Version:");
+		console.log(test.originalloadSettings.toString());
+	}
+}
+
+/*
 var A = require("../addons/testbehaviourA.js");
 var B = require("../addons/testbehaviourB.js");
 
@@ -10,4 +24,4 @@ exports.init= function(){
 		console.log("Modified behaviour");	
 	};
 	A.behaviour();
-};
+};*/
