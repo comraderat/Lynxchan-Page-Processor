4d1f0698caeebf66f1ef7535b1a16ea4f920a8d6
ComradeRat <comraderat@gmail.com>
2017-04-16 21:18:03 +0200

    wip

--- ProcessesingScripts/lynximport.js
+++ ProcessesingScripts/lynximport.js
@@ -1,9 +1,9 @@
 exports.Process = function(page){
 	debugger;
-	var xmltojs = require('xml2js')
+	var xmltojs = require('xml2js');
 
 	var xml = xmltojs.parseString(page, function(err, result){
-
+		var lol =1;
 	}
 
 	return page;
