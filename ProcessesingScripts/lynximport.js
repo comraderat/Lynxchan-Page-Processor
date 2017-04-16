bae813620b6d2419a0de1fcbcfd47474b6d78055
ComradeRat <comraderat@gmail.com>
2017-04-16 23:05:20 +0200

    hopefullydone

--- ProcessesingScripts/lynximport.js
+++ ProcessesingScripts/lynximport.js
@@ -1,14 +1,30 @@
+var templatehandler
+
 exports.Process = function(page){
 	debugger;
 	var jsdom = require('jsdom').jsdom;
+	var serializer = require('jsdom').serializeDocument;
 
 	var document = jsdom(page);
 
 	var list = document.getElementsByTagName("div");
 	
 	for(var i =0 ; i<list.length; i++){
-		
+		var path = list[i].getAttribute("path");
+		try{
+			var replacement = fs.readFileSync(path);
+		}catch(err){
+			console.log("Couldn't find file: " + path.toString());
+			debugger;
+		}
+		try{
+			replacement = replacement.toString();
+			replacement = jsdom(replacement);
+		}catch(err){
+			console.log("Couldn't make html from loaded file");
+			debugger;
+		}
+		list[i].parentNode.replaceChild(replacement, list[i]);
 	}
-	
-	return page;
+	return serializer(document);
 }
